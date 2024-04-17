84d16214f337a62dd63c51683790f4956d52cf9e
muxcmux <storm.bg@gmail.com>
2024-04-17 18:14:07 +0100

    chchchchcanges

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use anyhow::Result;
 use crossterm::{
     cursor,
-    event::{read, Event, KeyCode, KeyEvent},
-    terminal, ExecutableCommand, QueueableCommand, style::{self, Color},
+    event::{read, Event, KeyCode, KeyEvent, KeyEventKind},
+    style::{self, Color, Stylize},
+    terminal, ExecutableCommand, QueueableCommand,
 };
-use std::{io::Write, env};
-use anyhow::Result;
+use std::{env, io::Write};
 
 enum Mode {
     Normal,
@@ -18,8 +19,12 @@ enum Action {
     MoveRight,
     EnterNormalMode,
     EnterInsertModeAtCursor,
+    EnterInsertModeAfterCursor,
     AppendCharacter(char),
+    NewLine,
     InsertLineBelow,
+    InsertLineAbove,
+    DeleteCharacter,
     Quit,
 }
 
@@ -37,6 +42,9 @@ fn handle_normal_mode_key_event(event: &KeyEvent) -> Option<Action> {
         KeyCode::Char('k') => Some(Action::MoveUp),
         KeyCode::Char('l') => Some(Action::MoveRight),
         KeyCode::Char('i') => Some(Action::EnterInsertModeAtCursor),
+        KeyCode::Char('a') => Some(Action::EnterInsertModeAfterCursor),
+        KeyCode::Char('o') => Some(Action::InsertLineBelow),
+        KeyCode::Char('O') => Some(Action::InsertLineAbove),
         KeyCode::Char('q') => Some(Action::Quit),
         _ => None,
     }
@@ -45,8 +53,12 @@ fn handle_normal_mode_key_event(event: &KeyEvent) -> Option<Action> {
 fn handle_insert_mode_key_event(event: &KeyEvent) -> Option<Action> {
     match event.code {
         KeyCode::Esc => Some(Action::EnterNormalMode),
-        KeyCode::Char(c) => Some(Action::AppendCharacter(c)),
-        KeyCode::Enter => Some(Action::InsertLineBelow),
+        KeyCode::Char(c) => {
+            log::debug!("Char: {}", c);
+            None
+        },
+        KeyCode::Enter => Some(Action::NewLine),
+        KeyCode::Backspace => Some(Action::DeleteCharacter),
         _ => None,
     }
 }
@@ -82,12 +94,18 @@ impl Renderer {
         let curr_buffer = RenderBuffer::with_size(width, height);
         let prev_buffer = RenderBuffer::with_size(width, height);
 
-        Ok(Self { stdout, curr_buffer, prev_buffer, width, height })
+        Ok(Self {
+            stdout,
+            curr_buffer,
+            prev_buffer,
+            width,
+            height,
+        })
     }
 
     fn draw(&mut self, state: &Editor) -> Result<()> {
-        self.draw_buffer(state)?;
-        self.draw_statusline();
+        self.draw_buffer(state);
+        self.draw_statusline(state);
         self.paint()?;
         self.draw_cursor(state)?;
         self.stdout.flush()?;
@@ -95,24 +113,29 @@ impl Renderer {
     }
 
     fn paint(&mut self) -> Result<()> {
-        for Patch { cell: Cell { char, .. }, x, y } in self.curr_buffer.diff(&self.prev_buffer) {
+        let patches = self.curr_buffer.diff(&self.prev_buffer);
+        log::debug!("Patches: {}", patches.len());
+        for Patch {
+            cell: Cell { char, fg, bg },
+            x,
+            y,
+        } in patches
+        {
             self.stdout.queue(cursor::MoveTo(x as u16, y as u16))?;
-            self.stdout.queue(style::Print(char))?;
+            self.stdout.queue(style::PrintStyledContent(
+                char.with(fg.unwrap_or(Color::Reset))
+                    .on(bg.unwrap_or(Color::Reset)),
+            ))?;
         }
 
-        self.prev_buffer = std::mem::replace(&mut self.curr_buffer, RenderBuffer::with_size(self.width, self.height));
+        self.prev_buffer = std::mem::replace(
+            &mut self.curr_buffer,
+            RenderBuffer::with_size(self.width, self.height),
+        );
         Ok(())
     }
 
-    fn draw_buffer(&mut self, state: &Editor) -> Result<()> {
-        for x in 0..state.buffer_view.width {
-            for y in 0..state.buffer_view.height {
-                self.stdout.queue(cursor::MoveTo(x as u16, y as u16))?;
-                self.stdout.queue(style::Print(" "))?;
-            }
-        }
-
-        // draw the visible lines
+    fn draw_buffer(&mut self, state: &Editor) {
         for y in 0..state.buffer_view.height {
             match state.buffer.lines.get(state.buffer_view.buffer_start_y + y) {
                 None => break,
@@ -122,28 +145,45 @@ impl Renderer {
                         match line.chars().nth(buffer_x) {
                             None => break,
                             Some(c) => {
-                                self.stdout.queue(cursor::MoveTo(x as u16, y as u16))?;
-                                self.stdout.queue(style::Print(c))?;
+                                self.curr_buffer.put_char(c, x, y, None, None);
                             }
                         }
                     }
                 }
             }
         }
-
-        Ok(())
     }
 
-    fn draw_statusline(&mut self) {
-        // self.stdout.queue(cursor::MoveTo(0, (self.height - 1).try_into().unwrap()))?;
-        // self.stdout.queue(style::Print("Type 'q' to quit"))?;
-        let label = "Type 'q' to quit";
-        if label.len() < self.width {
+    fn draw_statusline(&mut self, state: &Editor) {
+        let label = match state.mode {
+            Mode::Normal => " NOR ",
+            Mode::Insert => " INS ",
+        };
+
+        let (cursor_x, cursor_y) = state.buffer_view.cursor_position_relative_to_buffer();
+        let position = format!(" {}:{} ", cursor_y + 1, cursor_x + 1);
+
+        if label.len() + position.len() < self.width {
             let chars: Vec<char> = label.chars().collect();
-            self.curr_buffer.put_chars(&chars, 0, self.height - 1, None, None);
-            for x in label.len()..self.width {
-                self.curr_buffer.put_char(' ', x, self.height - 1, None, None);
+            self.curr_buffer.put_chars(
+                &chars,
+                0,
+                self.height - 1,
+                Some(Color::Black),
+                Some(Color::White),
+            );
+            for x in label.len()..self.width - position.len() {
+                self.curr_buffer
+                    .put_char(' ', x, self.height - 1, None, None);
             }
+            let position_chars: Vec<char> = position.chars().collect();
+            self.curr_buffer.put_chars(
+                &position_chars,
+                self.width - position.len(),
+                self.height - 1,
+                Some(Color::Black),
+                Some(Color::White),
+            );
         }
     }
 
@@ -173,6 +213,7 @@ struct ScrollView {
     height: usize,
     cursor_x: usize,
     cursor_y: usize,
+    sticky_cursor_x: usize,
     buffer_start_y: usize,
     buffer_start_x: usize,
 }
@@ -184,23 +225,25 @@ impl ScrollView {
             height,
             cursor_x: 0,
             cursor_y: 0,
+            sticky_cursor_x: 0,
             buffer_start_y: 0,
             buffer_start_x: 0,
         }
     }
 
-    fn cursor_up(&mut self) {
+    fn adjust_cursor_x_after_vertical_move(&mut self, buffer: &Buffer) {
+        let (_cursor_x, cursor_y) = self.cursor_position_relative_to_buffer();
+        let current_line = buffer.lines.get(cursor_y).unwrap();
+        self.cursor_x = self.sticky_cursor_x.min(current_line.len());
+    }
+
+    fn cursor_up(&mut self, buffer: &Buffer) {
         if self.cursor_y > 0 {
             self.cursor_y = self.cursor_y.saturating_sub(1);
         } else {
             self.scroll_up();
         }
-    }
-
-    fn scroll_up(&mut self) {
-        if self.buffer_start_y > 0 {
-            self.buffer_start_y = self.buffer_start_y.saturating_sub(1);
-        }
+        self.adjust_cursor_x_after_vertical_move(buffer);
     }
 
     fn cursor_down(&mut self, buffer: &Buffer) {
@@ -211,6 +254,13 @@ impl ScrollView {
         } else {
             self.scroll_down(buffer)
         }
+        self.adjust_cursor_x_after_vertical_move(buffer);
+    }
+
+    fn scroll_up(&mut self) {
+        if self.buffer_start_y > 0 {
+            self.buffer_start_y = self.buffer_start_y.saturating_sub(1);
+        }
     }
 
     fn scroll_down(&mut self, buffer: &Buffer) {
@@ -220,11 +270,26 @@ impl ScrollView {
     }
 
     fn cursor_left(&mut self) {
-        self.cursor_x = self.cursor_x.saturating_sub(1);
+        if self.cursor_x > 0 {
+            self.cursor_x = self.cursor_x.saturating_sub(1);
+        } else {
+            //self.scroll_left();
+        }
+        self.sticky_cursor_x = self.cursor_x;
     }
 
-    fn cursor_right(&mut self) {
-        self.cursor_x += 1;
+    fn cursor_right(&mut self, buffer: &Buffer) {
+        let (cursor_x, cursor_y) = self.cursor_position_relative_to_buffer();
+        let current_line = buffer.lines.get(cursor_y).unwrap();
+
+        if self.cursor_x < self.width {
+            if cursor_x < current_line.len() {
+                self.cursor_x += 1;
+            }
+        } else {
+            //self.scroll_right(buffer);
+        }
+        self.sticky_cursor_x = self.cursor_x;
     }
 
     fn cursor_position_relative_to_buffer(&self) -> (usize, usize) {
@@ -259,24 +324,55 @@ impl Editor {
     }
 
     fn enter_normal_mode(&mut self) {
+        if let Mode::Insert = self.mode {
+            self.buffer_view.cursor_left();
+        }
         self.mode = Mode::Normal;
-        self.buffer_view.cursor_left();
     }
 
-    fn enter_insert_mode(&mut self) {
+    fn enter_insert_mode_relative_to_cursor(&mut self, x: usize) {
+        for _ in 0..x {
+            self.buffer_view.cursor_right(&self.buffer);
+        }
         self.mode = Mode::Insert;
     }
 
-    fn edit_buffer(&mut self, c: char) {
+    fn append_character(&mut self, c: char) {
         let (cursor_x, cursor_y) = self.buffer_view.cursor_position_relative_to_buffer();
         self.buffer.lines[cursor_y].insert(cursor_x, c);
-        self.buffer_view.cursor_right();
+        self.buffer_view.cursor_right(&self.buffer);
+    }
+
+    fn delete_character(&mut self) {
+        let (cursor_x, cursor_y) = self.buffer_view.cursor_position_relative_to_buffer();
+        if cursor_x > 0 {
+            self.buffer.lines[cursor_y].remove(cursor_x - 1);
+            self.buffer_view.cursor_left();
+        }
+    }
+
+    fn insert_line_break(&mut self) {
+        let (cursor_x, cursor_y) = self.buffer_view.cursor_position_relative_to_buffer();
+        let right = self.buffer.lines[cursor_y].split_off(cursor_x);
+        self.insert_line_relative_to_cursor(1, right);
     }
 
     fn insert_line_below(&mut self) {
-        self.buffer.lines.insert(self.buffer_view.cursor_y + 1, "".to_string());
-        self.buffer_view.cursor_down(&self.buffer);
+        self.insert_line_relative_to_cursor(1, "".to_string());
+    }
+
+    fn insert_line_above(&mut self) {
+        self.insert_line_relative_to_cursor(0, "".to_string());
+    }
+
+    fn insert_line_relative_to_cursor(&mut self, y: usize, contents: String) {
+        let (_, cursor_y) = self.buffer_view.cursor_position_relative_to_buffer();
+        self.buffer.lines.insert(cursor_y + y, contents);
+        for _ in 0..y {
+            self.buffer_view.cursor_down(&self.buffer);
+        }
         self.buffer_view.cursor_x = 0;
+        self.mode = Mode::Insert;
     }
 }
 
@@ -286,11 +382,15 @@ struct Buffer {
 
 impl Buffer {
     fn default() -> Self {
-        Self { lines: vec!["".to_string()] }
+        Self {
+            lines: vec!["".to_string()],
+        }
     }
 
     fn from_contents(contents: String) -> Self {
-        Self { lines: contents.lines().map(|l| l.to_string()).collect() }
+        Self {
+            lines: contents.lines().map(|l| l.to_string()).collect(),
+        }
     }
 }
 
@@ -317,7 +417,7 @@ struct Patch {
     y: usize,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct RenderBuffer {
     cells: Vec<Cell>,
     width: usize,
@@ -327,13 +427,18 @@ struct RenderBuffer {
 impl RenderBuffer {
     fn with_size(width: usize, height: usize) -> Self {
         let cells = vec![Cell::empty(); width * height];
-        Self { width, height, cells }
+        Self {
+            width,
+            height,
+            cells,
+        }
     }
 
     fn diff(&self, other: &Self) -> Vec<Patch> {
         assert!(self.width == other.width && self.height == other.height);
 
-        self.cells.iter()
+        self.cells
+            .iter()
             .zip(other.cells.iter())
             .enumerate()
             .filter(|(_, (a, b))| *a != *b)
@@ -373,38 +478,83 @@ impl RenderBuffer {
     }
 }
 
+fn setup_logging() -> Result<()> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}: {}",
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(fern::log_file("log.log")?)
+        .apply()?;
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
+    setup_logging()?;
+
     let mut renderer = Renderer::default()?;
 
-    let mut editor = Editor::with_size(
-        renderer.width,
-        renderer.height.saturating_sub(1),
-    )?;
+    let mut editor = Editor::with_size(renderer.width, renderer.height.saturating_sub(1))?;
 
     loop {
         renderer.draw(&editor)?;
 
         match read()? {
             Event::Key(event) => {
-                if let Some(action) = handle_key_event(&editor.mode, &event) {
-                    match action {
-                        Action::Quit                    => break,
-                        Action::MoveUp                  => { editor.buffer_view.cursor_up(); },
-                        Action::MoveDown                => { editor.buffer_view.cursor_down(&editor.buffer); },
-                        Action::MoveLeft                => { editor.buffer_view.cursor_left(); },
-                        Action::MoveRight               => { editor.buffer_view.cursor_right(); },
-                        Action::EnterInsertModeAtCursor => { editor.enter_insert_mode(); },
-                        Action::EnterNormalMode         => { editor.enter_normal_mode(); },
-                        Action::AppendCharacter(c)      => { editor.edit_buffer(c); },
-                        Action::InsertLineBelow         => { editor.insert_line_below(); },
+                if event.kind == KeyEventKind::Press {
+                    if let Some(action) = handle_key_event(&editor.mode, &event) {
+                        match action {
+                            Action::Quit => break,
+                            Action::MoveUp => {
+                                editor.buffer_view.cursor_up(&editor.buffer);
+                            }
+                            Action::MoveDown => {
+                                editor.buffer_view.cursor_down(&editor.buffer);
+                            }
+                            Action::MoveLeft => {
+                                editor.buffer_view.cursor_left();
+                            }
+                            Action::MoveRight => {
+                                editor.buffer_view.cursor_right(&editor.buffer);
+                            }
+                            Action::EnterInsertModeAtCursor => {
+                                editor.enter_insert_mode_relative_to_cursor(0);
+                            }
+                            Action::EnterInsertModeAfterCursor => {
+                                editor.enter_insert_mode_relative_to_cursor(1);
+                            }
+                            Action::EnterNormalMode => {
+                                editor.enter_normal_mode();
+                            }
+                            Action::NewLine => {
+                                editor.insert_line_break();
+                            }
+                            Action::InsertLineBelow => {
+                                editor.insert_line_below();
+                            }
+                            Action::InsertLineAbove => {
+                                editor.insert_line_above();
+                            }
+                            Action::AppendCharacter(c) => {
+                                editor.append_character(c);
+                            }
+                            Action::DeleteCharacter => {
+                                editor.delete_character();
+                            }
+                        }
                     }
                 }
             }
             Event::Resize(_, _) => todo!(),
-            Event::Mouse(_)     => todo!(),
-            Event::Paste(_)     => todo!(),
-            Event::FocusGained  => todo!(),
-            Event::FocusLost    => todo!(),
+            Event::Mouse(_) => todo!(),
+            Event::Paste(_) => todo!(),
+            Event::FocusGained => todo!(),
+            Event::FocusLost => todo!(),
         }
     }
 
